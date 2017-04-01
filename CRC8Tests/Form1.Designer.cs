4774bc69094408d440fd1a081a1e787dfad72c7e
Daniel <omnisurfer@gmail.com>
2017-04-01 12:09:07 -0700

    First Commit

---
+++ CRC8Tests/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label_naiveCRC = new System.Windows.Forms.Label();
+            this.label_CRC = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.sinBasisTextBox = new System.Windows.Forms.TextBox();
+            this.cosBasisTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sampleTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label_naiveCRC
+            // 
+            this.label_naiveCRC.AutoSize = true;
+            this.label_naiveCRC.Location = new System.Drawing.Point(12, 196);
+            this.label_naiveCRC.Name = "label_naiveCRC";
+            this.label_naiveCRC.Size = new System.Drawing.Size(35, 13);
+            this.label_naiveCRC.TabIndex = 0;
+            this.label_naiveCRC.Text = "label1";
+            // 
+            // label_CRC
+            // 
+            this.label_CRC.AutoSize = true;
+            this.label_CRC.Location = new System.Drawing.Point(72, 196);
+            this.label_CRC.Name = "label_CRC";
+            this.label_CRC.Size = new System.Drawing.Size(35, 13);
+            this.label_CRC.TabIndex = 1;
+            this.label_CRC.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Red;
+            this.pictureBox3.Location = new System.Drawing.Point(133, 196);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(175, 39);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox3_Paint);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(75, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(52, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(779, 519);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // sinBasisTextBox
+            // 
+            this.sinBasisTextBox.Location = new System.Drawing.Point(15, 280);
+            this.sinBasisTextBox.Multiline = true;
+            this.sinBasisTextBox.Name = "sinBasisTextBox";
+            this.sinBasisTextBox.Size = new System.Drawing.Size(226, 198);
+            this.sinBasisTextBox.TabIndex = 6;
+            // 
+            // cosBasisTextBox
+            // 
+            this.cosBasisTextBox.Location = new System.Drawing.Point(283, 280);
+            this.cosBasisTextBox.Multiline = true;
+            this.cosBasisTextBox.Name = "cosBasisTextBox";
+            this.cosBasisTextBox.Size = new System.Drawing.Size(226, 198);
+            this.cosBasisTextBox.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "SinBasis";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(280, 255);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "CosBasis";
+            // 
+            // sampleTextBox
+            // 
+            this.sampleTextBox.Location = new System.Drawing.Point(541, 280);
+            this.sampleTextBox.Multiline = true;
+            this.sampleTextBox.Name = "sampleTextBox";
+            this.sampleTextBox.Size = new System.Drawing.Size(226, 198);
+            this.sampleTextBox.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(538, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Samples";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 519);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sampleTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cosBasisTextBox);
+            this.Controls.Add(this.sinBasisTextBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label_CRC);
+            this.Controls.Add(this.label_naiveCRC);
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MinimumSize = new System.Drawing.Size(336, 274);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_naiveCRC;
+        private System.Windows.Forms.Label label_CRC;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox sinBasisTextBox;
+        private System.Windows.Forms.TextBox cosBasisTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox sampleTextBox;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CRC8Tests/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        ///
+        /// This enum is used to indicate what kind of checksum you will be calculating.
+        /// 
+        public enum CRC8_POLY
+        {
+            CRC8 = 0xD8,
+            CRC8_CCITT = 0x07,
+            CRC8_DALLAS_MAXIM = 0x31,
+            CRC8_SAE_J1850 = 0x1D,
+            CRC_8_WCDMA = 0x9b,
+        };
+
+        /// 
+        /// Class for calculating CRC8 checksums...
+        /// 
+        public class CRC8Calc
+        {
+            private byte[] table = new byte[256];
+
+            public byte Checksum(params byte[] val)
+            {
+                if (val == null)
+                    throw new ArgumentNullException("val");
+
+                byte c = 0;
+
+                foreach (byte b in val)
+                {
+                    c = table[c ^ b];
+                }
+
+                return c;
+            }
+
+            public byte[] Table
+            {
+                get
+                {
+                    return this.table;
+                }
+                set
+                {
+                    this.table = value;
+                }
+            }
+
+            public byte[] GenerateTable(CRC8_POLY polynomial)
+            {
+                byte[] csTable = new byte[256];
+
+                for (int i = 0; i < 256; ++i)
+                {
+                    int curr = i;
+
+                    for (int j = 0; j < 8; ++j)
+                    {
+                        if ((curr & 0x80) != 0)
+                        {
+                            curr = (curr << 1) ^ (int)polynomial;
+                        }
+                        else
+                        {
+                            curr <<= 1;
+                        }
+                    }
+
+                    csTable[i] = (byte)curr;
+                }
+
+                return csTable;
+            }
+
+            public CRC8Calc(CRC8_POLY polynomial)
+            {
+                this.table = this.GenerateTable(polynomial);
+            }
+        }
+
+        private static void WL(object text, params object[] args)
+        {
+            Console.WriteLine(text.ToString()); 
+            int temp = Convert.ToInt16(text.ToString());
+            Console.WriteLine("CRC " + Convert.ToString(temp, 16).PadLeft(2, '0'));            
+        }
+
+        public static void RunSnippet()
+        {
+            byte checksum;
+            byte[] testVal = new byte[] { 0x01 };
+            //CRC8Calc crc_dallas = new CRC8Calc(CRC8_POLY.CRC8_DALLAS_MAXIM);
+            //checksum = crc_dallas.Checksum(testVal);
+            //WL(checksum);
+            CRC8Calc crc = new CRC8Calc(CRC8_POLY.CRC8);
+            checksum = crc.Checksum(testVal);
+            WL(checksum);
+        }
+
+        byte POLYNOMIAL = 0xD8;
+
+        public byte crcNaive(byte message)
+        {
+            byte remainder;
+            /*
+             * Initially, the dividend is the remainder.
+             */
+            remainder = message;
+
+            /*
+             * For each bit position in the message....
+             */
+            for (uint bit = 8; bit > 0; --bit)
+            {
+                /*
+                 * If the uppermost bit is a 1...
+                 */
+                if ((byte)(remainder & 0x80) == 0x80)
+                {
+                    /*
+                     * XOR the previous remainder with the divisor.
+                     */
+                    remainder ^= POLYNOMIAL;
+                }
+
+                /*
+                 * Shift the next bit of the message into the remainder.
+                 */
+                remainder = (byte)(remainder << 1);
+            }
+
+            /*
+             * Return only the relevant bits of the remainder as CRC.
+             */
+            return (byte)(remainder >> 4);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int shiftValue = 0;
+
+        public void DrawLinesPointF(PaintEventArgs e, int shift)
+        {
+            Random random = new Random();
+
+            // Create pen.
+            Pen penSin = new Pen(Color.Red, 2), penCos = new Pen(Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255)), 2);
+
+            float yScale = 0.1F, yOffset = (pictureBox1.Bottom / 2), xScale = 2.0F, sinFreq = 1.0F, cosFreq = sinFreq * 1.0F, sinAmp = 2.5F, cosAmp = sinAmp * 1.0F;
+
+            PointF[] pointsSin = new PointF[pictureBox1.Right], pointsCos = new PointF[pictureBox1.Right];
+
+            for (int i = 0; i < pictureBox1.Right; i++)
+            {
+                pointsSin[i] = new PointF((i), (sinAmp * (yScale * pictureBox1.Bottom) * (float)Math.Sin((i * (sinFreq/(pictureBox1.Right/xScale)) * Math.PI) + shift)) + yOffset);                
+            }
+
+            for (int i = 0; i < pictureBox1.Right; i++)
+            {
+                pointsCos[i] = new PointF((i), (cosAmp * (yScale * pictureBox1.Bottom) * (float)Math.Cos((i * (cosFreq/(pictureBox1.Right/xScale)) * Math.PI) - (shift))) + yOffset);                              
+            }
+
+            //Draw lines to screen.
+            e.Graphics.DrawLines(penSin, pointsSin);
+
+            e.Graphics.DrawLines(penCos, pointsCos);
+        }
+
+        //private PictureBox pictureBox1 = new PictureBox();        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            int text = 0;
+            for (int i = 0; i < 256; i++)
+            {
+                text = crcNaive((byte)i);                              
+                Console.WriteLine("CRC Naive " + Convert.ToString(text, 16).PadLeft(2, '0'));                 
+            }
+
+            createData();
+
+            // Dock the PictureBox to the form and set its background to white.
+            //pictureBox1.Dock = DockStyle.Fill;
+            //pictureBox1.BackColor = Color.White;
+            
+            // Connect the Paint event of the PictureBox to the event handler method.
+            pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+
+            // Add the PictureBox control to the Form. 
+            //this.Controls.Add(pictureBox1);
+            
+            /*
+            try
+            {
+                RunSnippet();
+            }
+            catch (Exception x)
+            {
+                string error = string.Format
+                ("---\nThe following error occurred while executing the snippet:\n{0}\n---", e.ToString());
+                Console.WriteLine(error);
+            }
+             */ 
+        }
+
+        private void createData()
+        {
+            float[] samples = new float[1024], sinBasis = new float[1024], cosBasis = new float[1024];
+
+            float yScale = 0.1F, yOffset = (pictureBox1.Bottom / 2), xScale = 2.0F, sinFreq = 1.0F, cosFreq = sinFreq * 1.0F, sinAmp = 2.5F, cosAmp = sinAmp * 1.0F;
+
+            Random randomSample = new Random();
+
+            //simulated samples
+            for (int i = 0; i < samples.Length; i++)
+            {
+                samples[i] = (float)randomSample.NextDouble() * yScale * pictureBox1.Bottom * sinAmp;
+                sampleTextBox.AppendText(samples[i].ToString() + ",");
+            }
+
+            //sin & cos basis
+            for (int i = 0; i < sinBasis.Length; i++)
+            {
+                sinBasis[i] = (sinAmp * (yScale * pictureBox1.Bottom) * (float)Math.Sin((i * (sinFreq / (pictureBox1.Right / xScale)) * Math.PI)) + yOffset);
+                sinBasisTextBox.AppendText(sinBasis[i].ToString() + ",");
+
+                cosBasis[i] = (sinAmp * (yScale * pictureBox1.Bottom) * (float)Math.Cos((i * (sinFreq*100 / (pictureBox1.Right / xScale)) * Math.PI)) + yOffset);
+                cosBasisTextBox.AppendText(cosBasis[i].ToString() + ",");
+            }                        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            //pictureBox1.Invalidate(new Rectangle(100, 100, 100, 100));
+
+            pictureBox1.Invalidate();
+
+            pictureBox3.Hide();
+            
+            // Draw a line in the PictureBox.
+            //g.DrawLine(System.Drawing.Pens.Blue, pictureBox1.Left, pictureBox1.Top,
+                //pictureBox1.Right, pictureBox1.Bottom);
+        }
+
+        private void pictureBox1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {            
+            // Create a local version of the graphics object for the PictureBox.
+            Graphics g = e.Graphics;
+
+            Random random = new Random();             
+            
+            //System.Diagnostics.Debug.WriteLine("Box1 Event Triggered!");
+
+            int randomNumber = random.Next(0, pictureBox1.Bottom);
+
+            // Draw a line in the PictureBox.
+            g.DrawLine(System.Drawing.Pens.Purple, 0, (pictureBox1.Bottom / 2),
+                pictureBox1.Right, (pictureBox1.Bottom / 2));
+            
+            DrawLinesPointF(e, shiftValue);
+        }
+
+        private void pictureBox3_Paint(object sender, PaintEventArgs e)
+        {            
+            // Create a local version of the graphics object for the PictureBox.
+            Graphics g = e.Graphics;
+            
+            Random random = new Random();
+
+            int randomNumber = random.Next(0, pictureBox1.Bottom);
+
+            // Draw a line in the PictureBox.
+            g.DrawLine(System.Drawing.Pens.Blue, pictureBox1.Left, pictureBox1.Top,
+                pictureBox1.Right, randomNumber);                         
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            pictureBox3.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox3.Show();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (shiftValue > 255)
+                shiftValue = 0;
+
+            shiftValue++;
+
+            pictureBox1.Refresh();
+            pictureBox3.Refresh();
+
+            //System.Diagnostics.Debug.WriteLine("TimerTicked!");
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("MouseDown!");
+        }
+    }
+}
